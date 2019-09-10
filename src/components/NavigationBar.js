659ef4964d5f63383f23fbf930412c7787cca9e9
Kristophe <kristophe.lukas.sankar@gmail.com>
2019-09-10 12:32:43 -0400

    refactor: remove Component import

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Form from "react-bootstrap/Form"
