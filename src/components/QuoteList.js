93860bfe2efd7789ffb2bc1597e14b13de788946
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-10 22:07:27 -0400

    feat: update QuoteList styles

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -1,19 +1,25 @@
 import React from "react"
 import { connect } from "react-redux"
+import Card from "react-bootstrap/Card"
+import { TruncatedText } from "../styles/styles"
 
-const QuoteList = (props) => {
+const QuoteList = props => {
   const { quotes } = props
   return (
     <div>
-      { Object.values(quotes).map( (quote) => (
-          <div key={quote.id}>{quote.body}</div>
-        ))
-      }
+      {Object.values(quotes).map(quote => (
+        <Card className="m-2" key={quote.id}>
+          <Card.Body className="p-2">
+            <TruncatedText>{quote.body}</TruncatedText>
+            <Card.Link href="#">Preview</Card.Link>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   )
 }
 
-function mapStateToProps ({quotes}) {
+function mapStateToProps({ quotes }) {
   return {
     quotes
   }
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,3 +4,10 @@ export const Title = styled.h1`
   text-align: center;
   margin: 20px;
 `
+
+export const TruncatedText = styled.p`
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
