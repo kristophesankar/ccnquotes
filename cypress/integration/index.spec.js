688f645deccd542a28c2e925d2a03e5b485933cf
Kristophe <kristophe.lukas.sankar@gmail.com>
2019-09-13 10:56:07 -0400

    tests: update tests for AllQuotes page

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,14 +1,36 @@
-describe("CCNotes Quotes", () => {
+describe("Navigation Component", () => {
   before(() => {
     cy.visit("http://localhost:3000")
   })
-  it("Renders", () => {
+  it("Should render the NavigationBar", () => {
+    cy.get("nav")
+  })
+  it("Should show the app title", () => {
     cy.get("nav").contains("CCN Quotes")
   })
-  it("Renders", () => {
-    cy.get("div").should("have.class", "container")
+
+
+})
+
+describe("All Quotes Page", () => {
+  before(() => {
+    cy.visit("http://localhost:3000")
   })
-  it("Renders", () => {
-    cy.get("div.container").children()
+  it("Should show component title", () => {
+    cy.get("h1").contains('All Quotes')
+  })
+  it("Should show QuoteList container", () => {
+    cy.get("#quoteListColumn")
+  })
+  it("Should show QuoteList items", () => {
+    cy.get("#quoteListColumn").children()
+  })
+  it("Should show QuoteList Card content", () => {
+    cy.get("#quoteListColumn").children().children().each(($el, index, $list) => {
+      cy.get($el).should("have.class", 'card')
+      cy.get($el).children().should("have.class", 'card-body')
+      cy.get($el).children().find('p') // Should have preview text
+      cy.get($el).children().find('button') // Should have preview button
+    })
   })
 })
