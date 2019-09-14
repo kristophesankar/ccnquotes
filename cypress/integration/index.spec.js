4fabe49afdd979fc9502e410747972b03d8cbd33
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-14 16:56:04 -0400

    tests: test for an individual Quote Card details

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,35 +2,64 @@ describe("Navigation Component", () => {
   before(() => {
     cy.visit("http://localhost:3000")
   })
+
   it("Should render the NavigationBar", () => {
     cy.get("nav")
   })
+
   it("Should show the app title", () => {
     cy.get("nav").contains("CCN Quotes")
   })
-
-
 })
 
 describe("All Quotes Page", () => {
   before(() => {
     cy.visit("http://localhost:3000")
   })
+
   it("Should show component title", () => {
-    cy.get("h1").contains('All Quotes')
+    cy.get("h1").contains("All Quotes")
   })
+
   it("Should show QuoteList container", () => {
     cy.get("#quoteListColumn")
   })
+
   it("Should show QuoteList items", () => {
     cy.get("#quoteListColumn").children()
   })
+
   it("Should show QuoteList Card content", () => {
-    cy.get("#quoteListColumn").children().children().each(($el, index, $list) => {
-      cy.get($el).should("have.class", 'card')
-      cy.get($el).children().should("have.class", 'card-body')
-      cy.get($el).children().find('p') // Should have preview text
-      cy.get($el).children().find('button') // Should have preview button
-    })
+    cy.get("#quoteListColumn")
+      .children()
+      .children()
+      .each(($el, index, $list) => {
+        cy.get($el).should("have.class", "card")
+        cy.get($el)
+          .children()
+          .should("have.class", "card-body")
+        cy.get($el)
+          .children()
+          .find("p") // Should have preview text
+        cy.get($el)
+          .children()
+          .find("button") // Should have preview button
+      })
+  })
+
+  it("Should show QuoteCard", () => {
+    cy.get("#quoteCard")
+  })
+
+  it("Should show QuoteCard body", () => {
+    expect(cy.get("#entryBody")).to.not.equal("undefined")
+  })
+
+  it("Should show QuoteCard Author", () => {
+    expect(cy.get("#entryAuthor")).to.not.equal("undefined")
+  })
+
+  it("Should show QuoteCard Source", () => {
+    expect(cy.get("#entrySource")).to.not.equal("undefined")
   })
 })
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -11,15 +11,15 @@ class QuoteCard extends Component {
 
   render() {
     return (
-      <Card className="m-2">
+      <Card id="quoteCard" className="m-2">
         <Card.Body className="p-2">
           {this.props.quote.map(entry => (
             <div key={`quote-${entry.id}`}>
-              <div>{entry.body}</div>
+              <div id="entryBody">{entry.body}</div>
               <br />
-              <div>{entry.author}</div>
+              <div id="entryAuthor">{entry.author}</div>
               <br />
-              <div>{entry.source}</div>
+              <div id="entrySource">{entry.source}</div>
             </div>
           ))}
         </Card.Body>
