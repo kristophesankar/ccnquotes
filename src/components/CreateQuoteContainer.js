9bc5644cbef098108bd21e27d2e3d58ba5e16058
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-14 22:21:33 -0400

    feat: add CreateQuote form

--- src/components/CreateQuoteContainer.js
+++ src/components/CreateQuoteContainer.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import { Title } from "../styles/styles"
+import CreateQuoteForm from "./CreateQuoteForm"
 
 const CreateQuoteContainer = props => {
   return (
@@ -12,8 +13,10 @@ const CreateQuoteContainer = props => {
           <Title>Create Quote</Title>
         </Col>
       </Row>
-      <Row>
-        <Col sm={12}></Col>
+      <Row className="justify-content-center">
+        <Col sm={6}>
+          <CreateQuoteForm />
+        </Col>
       </Row>
     </Container>
   )
---
+++ src/components/CreateQuoteForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+class CreateQuoteForm extends Component {
+  state = {
+    body: "",
+    author: "",
+    source: ""
+  }
+
+  handleOnChangeBody = event => {
+    this.setState({
+      body: event.target.value
+    })
+  }
+
+  handleOnChangeAuthor = event => {
+    this.setState({
+      author: event.target.value
+    })
+  }
+
+  handleOnChangeSource = event => {
+    this.setState({
+      source: event.target.value
+    })
+  }
+
+  render() {
+    const { body, author, source } = this.state
+    return (
+      <Form>
+        <Form.Group controlId="formQuote">
+          <Form.Control
+            as="textarea"
+            rows="4"
+            placeholder="Enter quote text..."
+            onChange={this.handleOnChangeBody}
+            value={body}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formQuoteAuthor">
+          <Form.Control
+            type="text"
+            placeholder="Enter author..."
+            onChange={this.handleOnChangeAuthor}
+            value={author}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formQuoteSource">
+          <Form.Control
+            type="text"
+            placeholder="Enter source..."
+            onChange={this.handleOnChangeSource}
+            value={source}
+          />
+        </Form.Group>
+
+        <Row className="justify-content-center">
+          <Col className="text-center" sm={6}>
+            <Button variant="primary" className="m-1" type="">
+              Back
+            </Button>
+            <Button variant="primary" className="m-1" type="submit">
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    )
+  }
+}
+
+export default CreateQuoteForm
