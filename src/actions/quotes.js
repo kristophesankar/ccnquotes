5ffba482630ec1999df8481d5f26db0cdd0a170c
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-17 00:20:44 -0400

    feat: add update quote actions

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,6 +1,7 @@
 export const GET_QUOTES = "GET_QUOTES"
 export const GET_QUOTES_ERROR = "GET_QUOTES_ERROR"
 export const CREATE_QUOTE = "CREATE_QUOTE"
+export const UPDATE_QUOTE = "UPDATE_QUOTE"
 
 export function getQuotes(quotes) {
   return {
@@ -12,6 +13,13 @@ export function getQuotes(quotes) {
 export function createQuote(quote) {
   return {
     type: CREATE_QUOTE,
+    quote
+  }
+}
+
+export function updateQuote(quote) {
+  return {
+    type: UPDATE_QUOTE,
     quote
   }
 }
