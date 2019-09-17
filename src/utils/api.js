a45af4dff6aa92a4411cc71755fbb7fc10d62463
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-17 00:19:51 -0400

    feat: add update function to api utility

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,4 +25,13 @@ export function createNewQuote(data) {
   }).then(response => response.json())
 }
 
-
+// Update a new quote
+export function updateExistingQuote(data) {
+  return fetch(`http://localhost:3001/quotes/${data.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }).then(response => response.json())
+}
