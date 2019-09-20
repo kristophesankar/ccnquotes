d312364025fa6c764367ed05a7b92aad5cf719db
Kristophe <kristophe.lukas.sankar@gmail.com>
2019-09-20 12:06:51 -0400

    feat: add dispatch function on search event

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import { NavLink } from 'react-router-dom'
+import { handleSearchData } from '../actions/'
 
-const NavigationBar = () => {
-  return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='#home'>CCN Quotes</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='ml-auto'>
-          <Nav.Link className='showAllLink' as={NavLink} to='/'>
+class NavigationBar extends Component {
+  handleOnChangeSearch = (event) => {
+    event.preventDefault()
+    const { dispatch } = this.props
+    dispatch(handleSearchData(event.target.value))
+  }
+
+  render () {
+    return (
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>CCN Quotes</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link className='showAllLink' as={NavLink} to='/'>
             Show all
-          </Nav.Link>
-          <Nav.Link className='createLink' as={NavLink} to='/create'>
+            </Nav.Link>
+            <Nav.Link className='createLink' as={NavLink} to='/create'>
             Add +
-          </Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
-  )
+            </Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type='text' onChange={this.handleOnChangeSearch} placeholder='Search' className='mr-sm-2' />
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
 }
 
-export default NavigationBar
+export default connect()(NavigationBar)
