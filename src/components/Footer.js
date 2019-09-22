73b850849c20f1aa0f91734c2efdc1f027aa3c8f
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-22 19:21:58 -0400

    fix: wrap emoji in span

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = props => {
   return (
     <FooterRow className='justify-content-center'>
       <Col className='text-center' sm={12}>
-        <FooterText>Made with ❤️ by Arjuna Kristophe Sankar.</FooterText>
+        <FooterText>Made with <span aria-label='love-emoji' role='img'>❤️</span> by Arjuna Kristophe Sankar.</FooterText>
       </Col>
     </FooterRow>
   )
