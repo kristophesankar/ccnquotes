7eb14ac3e9ae03ae1f404e8a1efc81303f6bf9b6
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-22 17:09:37 -0400

    feat: add instructions to Create Quote page

--- src/components/CreateQuoteContainer.js
+++ src/components/CreateQuoteContainer.js
@@ -2,8 +2,17 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { Title } from '../styles/styles'
+import {
+  Title,
+  NavIcon,
+  PopTitle,
+  PopContent
+} from '../styles/styles'
+import { faInfoCircle, faSave, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import CreateQuoteForm from './CreateQuoteForm'
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import { Tooltip } from 'react-tippy'
 
 /*
  * This is a container for the create quote form.
@@ -14,7 +23,42 @@ const CreateQuoteContainer = props => {
     <Container id='createQuoteContainer'>
       <Row>
         <Col sm={12}>
-          <Title>Create Quote</Title>
+          <Title>Create Quote&nbsp;
+            <Tooltip
+              title='Click to show/hide instructions.'
+              position='bottom'
+              trigger='mouseenter'
+              animation='shift'
+            >
+              {/* This trigger shows the page instructions */}
+              <OverlayTrigger
+                trigger='click' placement='right' overlay={(
+                  <Popover id='allQuotesPopover'>
+                    <PopTitle>Instructions</PopTitle>
+                    <PopContent as='p'>
+                      <ul>
+                        <li>
+                          Fill out the form below by typing in your quote, its author
+                          and a source.
+                        </li><br />
+                        <li>
+                          Clicking on the <strong><NavIcon icon={faSave} /> Save </strong>
+                          button will store your entry.
+                        </li><br />
+
+                        <li>
+                          Clicking the <NavIcon icon={faChevronLeft} /> <strong>Back </strong>
+                          button will carry you to
+                          the last page you viewed.
+                        </li><br />
+                      </ul>
+                    </PopContent>
+                  </Popover>)}
+              >
+                <NavIcon size='xs' icon={faInfoCircle} />
+              </OverlayTrigger>
+            </Tooltip>
+          </Title>
         </Col>
       </Row>
       <Row className='justify-content-center'>
