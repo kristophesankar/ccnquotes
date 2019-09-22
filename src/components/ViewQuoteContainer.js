66b7bcb8dddbd6b014d759387035c2d5cad93b65
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-22 18:53:30 -0400

    feat: add instructions to Quote Actions page

--- src/components/ViewQuoteContainer.js
+++ src/components/ViewQuoteContainer.js
@@ -2,8 +2,23 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { Title } from '../styles/styles'
+import {
+  Title,
+  NavIcon,
+  PopTitle,
+  PopContent
+} from '../styles/styles'
 import ViewQuote from './ViewQuote'
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import { Tooltip } from 'react-tippy'
+import {
+  faInfoCircle,
+  faSave,
+  faChevronLeft,
+  faToggleOn,
+  faTrashAlt
+} from '@fortawesome/free-solid-svg-icons'
 
 /* Container for view quote page */
 const ViewQuoteContainer = props => {
@@ -11,7 +26,45 @@ const ViewQuoteContainer = props => {
     <Container id='viewQuoteContainer'>
       <Row>
         <Col sm={12}>
-          <Title>Quote Actions</Title>
+          <Title>Quote Actions&nbsp;
+            <Tooltip
+              title='Click to show/hide instructions.'
+              position='bottom'
+              trigger='mouseenter'
+              animation='shift'
+            >
+              {/* This trigger shows the page instructions */}
+              <OverlayTrigger
+                trigger='click' placement='right' overlay={(
+                  <Popover id='allQuotesPopover'>
+                    <PopTitle>Instructions</PopTitle>
+                    <PopContent as='p'>
+                      <ul>
+                        <li>
+                          Turn off the form guard by clicking&nbsp;
+                          <strong><NavIcon icon={faToggleOn} /> Toggle Editing</strong>.
+                        </li><br />
+                        <li>
+                          You can edit quote by making your changes and clicking<br />
+                          <strong><NavIcon icon={faSave} /> Update/Save</strong>.
+                        </li><br />
+                        <li>
+                          You can delete the quote clicking the&nbsp;
+                          <strong><NavIcon icon={faTrashAlt} /> Delete</strong> button.
+                        </li><br />
+                        <li>
+                          Clicking the <NavIcon icon={faChevronLeft} /> <strong>Back </strong>
+                          button will carry you to
+                          the last page you viewed.
+                        </li><br />
+                      </ul>
+                    </PopContent>
+                  </Popover>)}
+              >
+                <NavIcon size='xs' icon={faInfoCircle} />
+              </OverlayTrigger>
+            </Tooltip>
+          </Title>
         </Col>
       </Row>
       <Row className='justify-content-center'>
