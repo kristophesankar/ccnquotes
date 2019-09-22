92d577c9983c65f6ca9c178671c109b5bd72390d
kristophesankar <kristophe.lukas.sankar@gmail.com>
2019-09-22 15:57:16 -0400

    feat: add page instructions to AllQuotesContainer

--- src/components/AllQuotesContainer.js
+++ src/components/AllQuotesContainer.js
@@ -5,8 +5,19 @@ import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { Title, QuoteListContainer } from '../styles/styles'
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import {
+  Title,
+  DivContainer,
+  PanelTitle,
+  NavIcon,
+  PopTitle,
+  PopContent
+} from '../styles/styles'
 import { Scrollbars } from 'react-custom-scrollbars'
+import { faBookOpen, faListUl, faInfoCircle, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { Tooltip } from 'react-tippy'
 
 class AllQuotesContainer extends Component {
   state = {
@@ -23,16 +34,64 @@ class AllQuotesContainer extends Component {
       <Container id='allQuotesContainer'>
         <Row>
           <Col sm={12}>
-            <Title>All Quotes</Title>
+            <Title>
+              All Quotes&nbsp;
+
+              <Tooltip
+                title='Click to show/hide instructions.'
+                position='bottom'
+                trigger='mouseenter'
+                animation='shift'
+              >
+                {/* This trigger shows the page instructions */}
+                <OverlayTrigger
+                  trigger='click' placement='right' overlay={(
+                    <Popover id='allQuotesPopover'>
+                      <PopTitle>Instructions</PopTitle>
+                      <PopContent as='p'>
+                        <ul>
+                          <li>
+                            <strong>Click</strong> any card in the Quote List panel
+                            to show details.
+                          </li><br />
+                          <li>
+                            You can <strong>scroll</strong> on any panel when data
+                            is out of view.
+                          </li><br />
+                          <li>
+                            <strong>Click</strong> the <NavIcon icon={faEdit} /> to
+                            go to the Quote Actions page.
+                          </li><br />
+                        </ul>
+                      </PopContent>
+                    </Popover>)}
+                >
+                  <NavIcon size='xs' icon={faInfoCircle} />
+                </OverlayTrigger>
+              </Tooltip>
+            </Title>
           </Col>
+          <Col sm={12} style={{ textAlign: 'right' }} />
         </Row>
+
         <Row>
           <Col id='quoteListColumn' sm={6}>
+            <PanelTitle>
+              <Tooltip
+                title='All your saved quotes show up here!'
+                position='bottom'
+                trigger='mouseenter'
+                animation='shift'
+              >
+                <NavIcon icon={faListUl} /> Quote List
+              </Tooltip>
+            </PanelTitle>
+
             <Scrollbars
               autoHide autoHideTimeout={1000}
-              autoHideDuration={200} style={{ width: '100%', height: '80vh' }}
+              autoHideDuration={200} style={{ width: '100%', height: '65vh' }}
             >
-              <QuoteListContainer>
+              <DivContainer>
                 {/*
                   * Pass in the handleSelection function to the QuoteList component
                   */}
@@ -40,18 +99,35 @@ class AllQuotesContainer extends Component {
                   onHandleSelection={this.handleSelection}
                   className='qL'
                 />
-              </QuoteListContainer>
+              </DivContainer>
             </Scrollbars>
           </Col>
           <Col id='quoteCardColumn' sm={6}>
-            {/* Condition for showing selected card details */}
-            {this.state.selection === '' ? (
-              <Card className='m-2 quoteListItem'>
-                <Card.Body>Select a card to view its details...</Card.Body>
-              </Card>
-            ) : (
-              <QuoteCard quoteId={this.state.selection} />
-            )}
+            <PanelTitle>
+              <Tooltip
+                title='Select a Quote and it will show up below!'
+                position='bottom'
+                trigger='mouseenter'
+                animation='shift'
+              >
+                <NavIcon icon={faBookOpen} />  Quote Details
+              </Tooltip>
+            </PanelTitle>
+            <Scrollbars
+              autoHide autoHideTimeout={1000}
+              autoHideDuration={200} style={{ width: '100%', height: '65vh' }}
+            >
+              <DivContainer>
+                {/* Condition for showing selected card details */}
+                {this.state.selection === '' ? (
+                  <Card className='m-2 quoteListItem'>
+                    <Card.Body>Select a card to view its details...</Card.Body>
+                  </Card>
+                ) : (
+                  <QuoteCard quoteId={this.state.selection} />
+                )}
+              </DivContainer>
+            </Scrollbars>
           </Col>
         </Row>
       </Container>
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,6 +4,7 @@ import Navbar, { Toggle, Brand } from 'react-bootstrap/Navbar'
 import Card, { Body } from 'react-bootstrap/Card'
 import { Link } from 'react-bootstrap/Nav'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Title as PopoverTitle, Content as PopoverContent } from 'react-bootstrap/Popover'
 
 /* Colors */
 export const navTextColor = '#d1e8fa !important'
@@ -19,6 +20,12 @@ export const Title = styled.h1`
   font-weight: bold;
 `
 
+export const PanelTitle = styled.h5`
+  text-align: center;
+  margin: 10px;
+  font-weight: 600;
+`
+
 export const TruncatedText = styled.p`
   width: 100%;
   white-space: nowrap;
@@ -29,6 +36,21 @@ export const TruncatedText = styled.p`
 export const NavIcon = styled(FontAwesomeIcon)`
   color: ${navAccentColor};
   &:hover { cursor: pointer}
+`
+
+export const PopTitle = styled(PopoverTitle)`
+  background: inherit;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-family: Poppins;
+  font-weight: 600;
+`
+
+export const PopContent = styled(PopoverContent)`
+  background: inherit;
+  padding-top: 10px;
+  font-family: Poppins;
+  fon-size: inherit;
 `
 
 /* Navbar */
@@ -75,6 +97,6 @@ export const CustomCardBody = styled(Body)`
 
 /* AllQuotesContainer */
 
-export const QuoteListContainer = styled.div`
+export const DivContainer = styled.div`
   padding: 5px;
 `
